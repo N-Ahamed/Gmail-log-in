fce46f5dd5f5a02d8a56652de5d1168171120ec9
Nahid Ahamed <na601234@gmail.com>
2025-02-21 16:15:51 +0600

    Log in code

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/lib/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginScreen(),
+    );
+  }
+}
---
+++ static_data.dart
@@ -0,0 +1,20 @@
+import 'package:test_project/lib/model/user.dart';
+
+final List<User> users = [
+  User(
+    name: 'John Doe',
+    email: 'john@example.com',
+    phone: '123-456-7890',
+    description: 'A software engineer with 5 years of experience.',
+    profileImage:
+        'https://media.istockphoto.com/id/1355110818/photo/studio-shot-of-a-handsome-and-happy-young-man-posing-against-a-grey-background.jpg?s=612x612&w=0&k=20&c=T39jUOOjC8H-Op0cfd-uiNXk1a2XBn1sXkQbKIWwY7E=',
+  ),
+  User(
+    name: 'Jane Smith',
+    email: 'jane@example.com',
+    phone: '987-654-3210',
+    description: 'A product designer specializing in UX/UI.',
+    profileImage:
+        'https://img.freepik.com/free-photo/portrait-hapy-young-smart-man-glasses-white-wall_231208-12206.jpg',
+  ),
+];
